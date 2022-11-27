be03f24eabd73e9232dba0e26051e2dfcdd72312
Adam Fowler <cockytrumpet@icloud.com>
2022-11-27 19:00:18 +0000

    decision tree and matrix PNGs

--- code/tree.py
+++ code/tree.py
@@ -1,76 +1,76 @@
 import pandas as pd
 import numpy as np
 import graphviz
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
-data = pd.read_csv("datasets/dataset.csv", index_col=0)
 
-"""
-Balancing the dataset reduces the accuracy for increases in precision and recall (worth?)
+def make_tree(data, matrix_title, output_file):
+    target = data["HeartDiseaseorAttack"]
+    features = data.drop("HeartDiseaseorAttack", axis=1)
 
-Unbalanced:
-              precision    recall  f1-score   support
+    features_train, features_test, target_train, target_test = train_test_split(
+        features, target, test_size=0.2, random_state=42
+    )
 
-         0.0       0.91      0.99      0.95     45968
-         1.0       0.57      0.09      0.15      4768
+    classifier = tree.DecisionTreeClassifier(
+        max_depth=9, min_samples_leaf=4, random_state=42, criterion="entropy"
+    )
+    classifier.fit(features_train, target_train)
+    result = classifier.predict(features_test)
+    matrix = metrics.confusion_matrix(target_test, result)
 
-    accuracy                           0.91     50736
-   macro avg       0.74      0.54      0.55     50736
-weighted avg       0.88      0.91      0.88     50736
+    # save matrix to png
+    names = ["True Negative", "False Positive", "False Negative", "True Positive"]
+    counts = [f"{x:0.0f}" for x in matrix.flatten()]
+    percents = [f"{x:.2%}" for x in matrix.flatten() / np.sum(matrix)]
+    labels = [f"{n}\n{c}\n{p}" for n, c, p in zip(names, counts, percents)]
+    labels = np.asarray(labels).reshape(2, 2)
+    plt.figure(figsize=(4, 4))
+    pretty_matrix = sns.heatmap(matrix, annot=labels, fmt="")
+    pretty_matrix.set(title=matrix_title)
+    pretty_matrix.set(xlabel="Predicted", ylabel="Actual")
+    plt.savefig(f"{output_file}_matrix")
 
-Balanced:
-              precision    recall  f1-score   support
+    # print stats
+    importance = pd.DataFrame(
+        {
+            "feature": features_train.columns,
+            "importance": np.round(classifier.feature_importances_, 3),
+        }
+    )
+    importance.sort_values("importance", ascending=False, inplace=True)
+    print(f"{matrix_title}")
+    print(metrics.classification_report(target_test, result))
+    print(importance)
 
-         0.0       0.79      0.72      0.75      4818
-         1.0       0.74      0.80      0.77      4740
+    # save decision tree as png
+    dot_data = tree.export_graphviz(
+        classifier,
+        out_file=None,
+        feature_names=features_train.columns,
+        filled=True,
+        proportion=True,
+        rounded=True,
+        max_depth=3,
+        rotate=True,
+    )
+    graph = graphviz.Source(dot_data)
+    graph.format = "png"
+    graph.render(output_file)
 
-    accuracy                           0.76      9558
-   macro avg       0.76      0.76      0.76      9558
-weighted avg       0.76      0.76      0.76      9558
-"""
-pos_sample = data[data["HeartDiseaseorAttack"] == 1]
-neg_sample = data[data["HeartDiseaseorAttack"] == 0].sample(n=len(pos_sample))
-data = pd.concat([pos_sample, neg_sample], ignore_index=True)
-data = data.sample(frac=1)
 
-target = data["HeartDiseaseorAttack"]
-features = data.drop("HeartDiseaseorAttack", axis=1)
+data_original = pd.read_csv("datasets/dataset.csv", index_col=0)
 
-features_train, features_test, target_train, target_test = train_test_split(
-    features, target, test_size=0.2, random_state=42
+pos_sample = data_original[data_original["HeartDiseaseorAttack"] == 1]
+neg_sample = data_original[data_original["HeartDiseaseorAttack"] == 0].sample(
+    n=len(pos_sample)
 )
+data_balanced = pd.concat([pos_sample, neg_sample], ignore_index=True)
+data_balanced = data_balanced.sample(frac=1)
 
-classifier = tree.DecisionTreeClassifier(
-    max_depth=9, min_samples_leaf=4, random_state=42, criterion="entropy"
-)
-classifier.fit(features_train, target_train)
-result = classifier.predict(features_test)
-matrix = metrics.confusion_matrix(target_test, result)
-
-print("Confusion matrix")
-print(matrix)
-print(metrics.classification_report(target_test, result))
-
-importance = pd.DataFrame(
-    {
-        "feature": features_train.columns,
-        "importance": np.round(classifier.feature_importances_, 3),
-    }
-)
-importance.sort_values("importance", ascending=False, inplace=True)
-print(importance)
-
-dot_data = tree.export_graphviz(
-    classifier,
-    out_file=None,
-    feature_names=features_train.columns,
-    filled=True,
-    proportion=True,
-    rounded=True,
-    max_depth=4,
-)
-graph = graphviz.Source(dot_data)
-graph.format = "png"
-graph.render("tree")
+make_tree(data_original, "Original", "tree")
+make_tree(data_balanced, "Balanced", "tree_balanced")
--- preliminary_results.md
+++ preliminary_results.md
@@ -46,38 +46,69 @@ weighted avg       0.89      0.91      0.87     50736
 # Decision Tree
 ```
 
-Confusion matrix
-[[45480   488]
- [ 4273   495]]
+Confusion Matrix - Original
               precision    recall  f1-score   support
 
          0.0       0.91      0.99      0.95     45968
-         1.0       0.50      0.10      0.17      4768
+         1.0       0.57      0.09      0.15      4768
 
     accuracy                           0.91     50736
-   macro avg       0.71      0.55      0.56     50736
+   macro avg       0.74      0.54      0.55     50736
 weighted avg       0.88      0.91      0.88     50736
 
               feature  importance
-11            GenHlth       0.347
-14                Age       0.210
-4              Stroke       0.108
-13                Sex       0.066
-1            HighChol       0.057
-0              HighBP       0.053
-16             Income       0.022
-3              Smoker       0.020
-15          Education       0.018
-20       Diabetes_bin       0.018
-12           DiffWalk       0.016
-17            BMI_bin       0.015
-18       MentHlth_bin       0.010
-19       PhysHlth_bin       0.010
-6              Fruits       0.007
-10        NoDocbcCost       0.006
-8   HvyAlcoholConsump       0.004
-7             Veggies       0.004
-5        PhysActivity       0.004
-9       AnyHealthcare       0.003
+11            GenHlth       0.372
+14                Age       0.294
+0              HighBP       0.075
+4              Stroke       0.070
+13                Sex       0.062
+1            HighChol       0.056
+3              Smoker       0.013
+16             Income       0.012
+20       Diabetes_bin       0.012
+12           DiffWalk       0.010
+15          Education       0.005
+17            BMI_bin       0.004
+18       MentHlth_bin       0.003
+19       PhysHlth_bin       0.003
+9       AnyHealthcare       0.002
+7             Veggies       0.002
+5        PhysActivity       0.002
+10        NoDocbcCost       0.002
+8   HvyAlcoholConsump       0.001
+6              Fruits       0.001
+2           CholCheck       0.001
+
+Confusion Matrix - Balanced
+              precision    recall  f1-score   support
+
+         0.0       0.78      0.72      0.75      4825
+         1.0       0.73      0.80      0.76      4733
+
+    accuracy                           0.76      9558
+   macro avg       0.76      0.76      0.76      9558
+weighted avg       0.76      0.76      0.76      9558
+
+              feature  importance
+11            GenHlth       0.376
+14                Age       0.295
+0              HighBP       0.061
+1            HighChol       0.058
+13                Sex       0.058
+4              Stroke       0.048
+16             Income       0.018
+12           DiffWalk       0.015
+3              Smoker       0.014
+15          Education       0.010
+20       Diabetes_bin       0.009
+17            BMI_bin       0.007
+6              Fruits       0.005
+18       MentHlth_bin       0.005
+10        NoDocbcCost       0.004
+7             Veggies       0.003
+5        PhysActivity       0.003
 2           CholCheck       0.003
+19       PhysHlth_bin       0.003
+8   HvyAlcoholConsump       0.002
+9       AnyHealthcare       0.001
 ```
