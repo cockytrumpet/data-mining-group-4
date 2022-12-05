714fc599d6750c79a396c6854657791d04b4074e
jacob-s-unger <jacob.s.unger@gmail.com>
2022-12-05 14:45:36 -0700

    .

--- predictor/flask_app.py
+++ predictor/flask_app.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods =["GET", "POST"])
 def home():
    return render_template('home.html')
 
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
-   
-   output = request.form.to_dict()
-
    return render_template('result_value.html')
 
 if __name__ == '__main__':
