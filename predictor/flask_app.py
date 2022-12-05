cf7cf1ee744da445576b2d8ed0f29193cfb2132f
jacob-s-unger <jacob.s.unger@gmail.com>
2022-12-05 14:01:10 -0700

    flask mods to python tie in

--- predictor/flask_app.py
+++ predictor/flask_app.py
@@ -6,5 +6,12 @@ def home():
    return render_template('home.html')
 
 
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   
+   output = request.form.to_dict()
+
+   return render_template('result_value.html')
+
 if __name__ == '__main__':
    app.run(debug = True)
