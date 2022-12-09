a7907b19ed7da39e77cfffef2cd24490db03fdef
Adam Fowler <cockytrumpet@icloud.com>
2022-12-09 11:33:05 -0500

    fixed web_app crash on empty form

--- web_app_server/build/python-docker/app.py
+++ web_app_server/build/python-docker/app.py
@@ -2,9 +2,14 @@ import pickle
 
 import pandas as pd
 from flask import Flask, flash, render_template, request
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+# not secret, this is needed for flash()
 app.config["SECRET_KEY"] = "28185ab68f8cb88d4a7f09d7cc9ed27616d610a23be767dc"
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 
 @app.route("/")
@@ -30,7 +35,8 @@ def result():
             return render_template("form.html")
 
 
-with open("models/tree_balanced.model", "rb") as f:
+with open("./tree_balanced.model", "rb") as f:
     loaded_model = pickle.load(f)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
